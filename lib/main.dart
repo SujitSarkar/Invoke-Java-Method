fc577969db7757bc416d8ebef4a25ae397f4b6ae
Sujit Sarkar <snigdho532@gmail.com>
2023-12-05 12:45:02 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _batteryLevel = 'Unknown battery level.';
+  static const channel = "samples.flutter.dev/battery";
+  static const platform = MethodChannel(channel);
+
+
+  Future<void> getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result%';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Invoke java method'),centerTitle: true),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  await getBatteryLevel();
+                },
+                child: const Text('Get Battery Level'),
+              ),
+              Text(_batteryLevel),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Sky extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Rect rect = Offset.zero & size;
+    const RadialGradient gradient = RadialGradient(
+      center: Alignment(0.7, -0.6),
+      radius: 0.5,
+      colors: <Color>[Color(0xFFFFFF00), Color(0xFF0099FF)],
+      stops: <double>[0.4, 1.0],
+    );
+    canvas.drawRect(
+      rect,
+      Paint()..shader = gradient.createShader(rect),
+    );
+  }
+
+  @override
+  SemanticsBuilderCallback get semanticsBuilder {
+    return (Size size) {
+      Rect rect = Offset.zero & size;
+      final double width = size.shortestSide * 0.4;
+      rect = const Alignment(0.8, -0.9).inscribe(Size(width, width), rect);
+      return <CustomPainterSemantics>[
+        CustomPainterSemantics(
+          rect: rect,
+          properties: const SemanticsProperties(
+            label: 'Sun',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+      ];
+    };
+  }
+
+  @override
+  bool shouldRepaint(Sky oldDelegate) => false;
+  @override
+  bool shouldRebuildSemantics(Sky oldDelegate) => false;
+}
